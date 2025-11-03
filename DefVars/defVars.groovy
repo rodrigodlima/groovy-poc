02e92951792370d6e32bee967bebfb5a262eb969
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2025-11-02 21:45:20 -0300

    Fix code adding break

--- DefVars/defVars.groovy
+++ DefVars/defVars.groovy
@@ -1,13 +1,16 @@
 age = 5
 checkVar(age)
 name = "jose"
+checkVar(name)
 
 def checkVar(variable) {
     switch(variable.getClass()) {
         case Integer:
             println("$variable was evaluated as Integer")
+            break
         case String:
             println("$variable was evaluated as String")
+            break
         break
     }
 }
