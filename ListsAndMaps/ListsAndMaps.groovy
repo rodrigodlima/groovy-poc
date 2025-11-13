5993d8777392713307424e5bc27edad4e361c005
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2025-11-12 21:38:44 -0300

    Test map and lists in Groovy

---
+++ ListsAndMaps/ListsAndMaps.groovy
@@ -0,0 +1,11 @@
+def myList = [1,2,3,4,5]
+println "List: " + myList
+
+def myMap = [name: 'Groovy', type: 'Programming Language']
+println "Map: " + myMap
+
+def getFirstListValue = myList[0]
+println "First element on list: " + getFirstListValue
+
+def getValueFromMap = myMap['name']
+println getValueFromMap
