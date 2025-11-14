8b415e4c92084bd1ae84c70d835c73b416fd1285
Rodrigo de Lima Silva <rodrigodlima@gmail.com>
2025-11-14 06:46:19 -0300

    Add newline and enhance number list iteration in closuresExamples.groovy

--- Clousures/clousuresExamples.groovy
+++ Clousures/clousuresExamples.groovy
@@ -7,3 +7,6 @@ sayHello("Artur")
 def squareResult = {int number -> number * number }
 def result = squareResult(5)
 println ("Square of 5 is $result")
+
+def numberList = [1,2,3,4,5]
+numberList.each { number -> println("Number: $number")}
